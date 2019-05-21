d9d4343dc14c63d2fec97c614959ada67f4eb501
Mathieu8 <mathieuvanommeren@gmail.com>
2019-05-21 11:47:19 +0200

    Initial commit

---
+++ projectA/src/main/java/projectA/ProjectA.java
@@ -0,0 +1,17 @@
+package projectA;
+
+import projectB.ProjectB;
+
+public class ProjectA {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ProjectA().printProjectA();
+		new ProjectB().printProjectB();
+
+	}
+
+	public void printProjectA() {
+		System.out.println("In project A");
+	}
+	
+}
---
+++ projectB/src/main/java/projectB/ProjectB.java
@@ -0,0 +1,15 @@
+package projectB;
+
+public class ProjectB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ProjectB().printProjectB();
+
+	}
+	
+	public void printProjectB() {
+		System.out.println("In project B");
+	}
+
+}
